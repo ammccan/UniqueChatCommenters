799b9c0fdb55a37220fa175ba4579dfc08cefba6
ammccan <37963858+ammccan@users.noreply.github.com>
2018-09-25 12:30:54 -0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,44 @@
+import json, os
+import pandas as pd
+import numpy as np
+from collections import Counter
+TextFiles = []
+FName = []
+csv_rows = []
+commenterid = []
+unique_id = []
+NC = []
+for root, dirs, files in os.walk("/Users/ammcc/Desktop/"):
+    for file in files:
+        if file.endswith("chatinfo.txt"):
+            path = "/Users/ammcc/Desktop/"
+            filepath = os.path.join(path,file)
+            head, filename = os.path.split(filepath)
+            TextFiles.append(filepath)
+            FName.append(filename)
+            
+            n_commenters = 0
+            with open(filepath) as open_file:
+                for line in open_file:
+                    jsondata = json.loads(line)
+                    if "commenter" in jsondata:
+                        commenterid.append(jsondata["commenter"]["_id"])
+                        
+                        list_set = set(commenterid)
+                        unique_list = (list(list_set))
+
+                    for x in list_set:
+                        n_commenters += 1
+                        
+                        commenterid.clear()
+                csv_rows.append([filename, n_commenters])
+df = pd.DataFrame(csv_rows, columns=['FileName', 'Unique_Commenters'])
+df.to_csv('CommeterID.csv', index=False)
+
+
+
+        
+                    
+                    
+        
+    
